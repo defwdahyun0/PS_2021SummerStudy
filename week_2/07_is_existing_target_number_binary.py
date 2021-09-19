b2d3dc46a533a9a595dc1a4994478310dd3f6b91
defwdahyun0 <wdh112139@gmail.com>
2021-09-19 20:46:12 +0900

    2주차 수업

---
+++ week_2/07_is_existing_target_number_binary.py
@@ -0,0 +1,11 @@
+finding_target = 2
+finding_numbers = [0, 3, 5, 6, 1, 2, 4]
+
+def is_exist_target_number_binary(target, numbers):
+    # 이 부분을 채워보세요! 
+    # 무작위로 정렬되어 있는 배열에서는 이진 탐색을 사용할 수 없습니다!
+    return 1
+
+
+result = is_exist_target_number_binary(finding_target, finding_numbers)
+print(result)
