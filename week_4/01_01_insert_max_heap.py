41ef202d16a34be6ae3dce5244bcbc352b138e43
defwdahyun0 <wdh112139@gmail.com>
2021-09-20 18:11:07 +0900

    4주차 수업

---
+++ week_4/01_01_insert_max_heap.py
@@ -0,0 +1,27 @@
+class MaxHeap:
+    def __init__(self):
+        self.items = [None]
+
+    # 1. 새노드를 맨 끝에 추가
+    # 2. 새노드를 부모와 비교. 부모보다 크다면 교체
+    # 3. 꼭대기까지 반복
+
+    def insert(self, value):
+        self.items.append(value)
+        cur_index = len(self.items) - 1
+
+        while cur_index > 1:  # cur_index 가 1이 되면 정상을 찍은거라 다른 것과 비교 안하셔도 됩니다!
+            parent_index = cur_index // 2
+            if self.items[parent_index] < self.items[cur_index]:
+                self.items[parent_index], self.items[cur_index] = self.items[cur_index], self.items[parent_index]
+                cur_index = parent_index
+            else:
+                break
+
+
+max_heap = MaxHeap()
+max_heap.insert(3)
+max_heap.insert(4)
+max_heap.insert(2)
+max_heap.insert(9)
+print(max_heap.items)  # [None, 9, 4, 2, 3] 가 출력되어야 합니다!
